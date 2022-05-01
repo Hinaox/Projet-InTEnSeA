baa0d5673a8da48df790c45b4d1cb218fb47e073
Hinaox <Hiaronathanael178@gmail.com>
2022-05-02 00:21:44 +0300

    create project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'intensea',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
